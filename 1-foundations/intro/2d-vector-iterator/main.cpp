aa2492a218fd0e9f06281db20a02e62b4c047364
Peter Garside-Quin <petegq@gmail.com>
2022-07-18 15:24:23 +0100

    Iterate over 2d vector, solution using auto and ranges

--- 1-foundations/intro/2d-vector-iterator/main.cpp
+++ 1-foundations/intro/2d-vector-iterator/main.cpp
@@ -4,9 +4,7 @@ using std::cout;
 using std::vector;
 
 int main() {
-    vector<vector<int>> b {{1, 2},
-                           {3, 4},
-                           {5, 6}};
+    vector<vector<int>> b{{1, 2}, {3, 4}, {5, 6}};
 
     // Write your double loop over the above 2d vector here.
     for (int i = 0; i < b.size(); i++) {
@@ -14,5 +12,12 @@ int main() {
             cout << b[i][y] << "\n";
         }
     }
-    
+
+    // Solution, using auto and ranges
+    // for (auto v : b) {
+    //     for (int i : v) {
+    //         cout << i << " ";
+    //     }
+    //     cout << "\n";
+    // }
 }
