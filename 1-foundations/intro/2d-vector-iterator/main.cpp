34027d46fb49e98755f2ea235da71ea87b2f77f8
Peter Garside-Quin <petegq@gmail.com>
2022-07-18 15:20:24 +0100

    Iterate over 2d vector

---
+++ 1-foundations/intro/2d-vector-iterator/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::vector;
+
+int main() {
+    vector<vector<int>> b {{1, 2},
+                           {3, 4},
+                           {5, 6}};
+
+    // Write your double loop over the above 2d vector here.
+    for (int i = 0; i < b.size(); i++) {
+        for (int y = 0; y < b[i].size(); y++) {
+            cout << b[i][y] << "\n";
+        }
+    }
+    
+}
