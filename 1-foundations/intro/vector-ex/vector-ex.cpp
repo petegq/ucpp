d2669b72bdd3b0b72d2ca85c78b990eae4e6ef1e
Peter Garside-Quin <petegq@gmail.com>
2022-07-18 13:33:11 +0100

    2d vector exercise

---
+++ 1-foundations/intro/vector-ex/vector-ex.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+using std::vector;
+
+int main() {
+    vector<vector<int>> board = {{0, 1, 0, 0, 0, 0},
+                                 {0, 1, 0, 0, 0, 0},
+                                 {0, 1, 0, 0, 0, 0},
+                                 {0, 1, 0, 0, 0, 0},
+                                 {0, 0, 0, 0, 1, 0}};
+}
