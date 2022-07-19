a2bde621cbb4eba8a1dd2049e5d4f16a1fbc81e6
Peter Garside-Quin <petegq@gmail.com>
2022-07-19 20:26:00 +0100

    Add note

--- 1-foundations/intro/reading-streams/main.cpp
+++ 1-foundations/intro/reading-streams/main.cpp
@@ -13,3 +13,10 @@ int main() {
         }
     }
 }
+
+// Recap
+// -----
+// 1. #include <fstream>
+// 2. Create a std::ifstream object using the path to your file.
+// 3. Evaluate the std::ifstream object as a bool to ensure that the stream creation did not fail.
+// 4. Use a while loop with getline to write file lines to a string.
