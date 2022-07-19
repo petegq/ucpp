bce99d4d0d17da0f16c94de85ab27d9f88930cfc
Peter Garside-Quin <petegq@gmail.com>
2022-07-19 19:56:10 +0100

    While if conditional

---
+++ 1-foundations/intro/while-if/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cout;
+
+int main() {
+    int i = 1;
+    
+    while (i < 11) {
+        if (i % 2 == 0) {
+            cout << i << "\n";
+        }
+        i++;
+    }
+}
